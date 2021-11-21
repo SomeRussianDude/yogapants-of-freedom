51d47f83b49413be7f9e4a09122fd7c647423162
SomeRussianDude <yellow.mockingbird@gmail.com>
2021-11-21 09:35:56 +0100

    Initial commit, adding project

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,15)]   [SerializeField] string storyText;
+    [SerializeField] State[] nextState;
+
+    public string GetStateStory()
+    {
+        return storyText; 
+    }
+    public State[] GetNextState()
+    {
+        return nextState;
+    }
+}
